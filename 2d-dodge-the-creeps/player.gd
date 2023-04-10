665d8d4e7f9e7bb9b1dc375d78a55fd1d152d5fb
nndwn <nandawan.libya@gmail.com>
2023-04-11 02:39:26 +0700

    update

--- 2d-dodge-the-creeps/player.gd
+++ 2d-dodge-the-creeps/player.gd
@@ -1,4 +1,4 @@
-extends Area2D 
+extends Area2D
 
 export var speed = 400.0 #export menampilkan input configuration pada game engine dengan nilai default 400
 var screen_size = Vector2.ZERO #menampikan nilai x = 0 dan y = 0 
@@ -17,7 +17,20 @@ func _process(delta): #_process adalah function defaault dari godot untuk mempro
 		direction.y += 1
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
-	if direction.length() > 1: #jika total jumlah direction lebih  dari satu maka true 
+	if direction.length() > 0: #jika total jumlah direction lebih  dari satu maka true 
 		direction = direction.normalized() #sebuh fungsi normalised directon sehingga tombol atau key hany dapat berjalaan salah satu
+		$AnimatedSprite.play() # get_node("AnimatedSprite").play()
+	else:
+		$AnimatedSprite.stop()
 	position += direction * speed * delta #buaat object menjadi berpindah posisi berdasarkan nilai key
+	position.x =  clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if direction.x != 0:
+		$AnimatedSprite.animation ="right"
+		$AnimatedSprite.flip_h = direction.x < 0
+		$AnimatedSprite.flip_v = false
+	elif direction.y !=0:
+		$AnimatedSprite.animation ="up"
+		$AnimatedSprite.flip_v = direction.y > 0
 	
